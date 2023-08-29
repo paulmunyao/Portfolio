b21b3454ffb38271b9f79df9ad6fe0bdddbcb68d
paulmunyao <paulmunyao094@gmail.com>
2023-08-29 16:13:58 +0300

    add routing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResearchComponent } from './research/research.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'research', component:ResearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
