76d43b25db7cc89184df6727e0002eff182fb36f
paulmunyao <paulmunyao094@gmail.com>
2023-08-31 15:38:27 +0300

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ResearchComponent } from './research/research.component';
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'projects', component:ProjectsComponent},
-  {path:'research', component:ResearchComponent}
+  {path:'research', component:ResearchComponent},
+  {path:'blog', component:BlogComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ResearchComponent } from './research/research.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProjectsComponent,
-    ResearchComponent
+    ResearchComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
